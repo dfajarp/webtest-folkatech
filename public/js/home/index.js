60520e406606e5a3f91996105951f0eb627a22ea
fajar <dikfajar06@gmail.com>
2023-03-02 10:38:01 +0700

    perbaikan index.js

--- public/js/home/index.js
+++ public/js/home/index.js
@@ -28,14 +28,16 @@ const sendCard = (v) => {
             'http://localhost:8080/img/' + v.picture
           }" alt="image product">
           <div class="px-2 py-2">
-            <div class="font-bold text-sm mb-2">${v.name}</div>
-            <span class="flex space-x-1 items-center text-xs text-green-500">
+            <div class="flex justify-start font-bold text-sm mb-2">${
+              v.name
+            }</div>
+            <span class="flex space-x-1 text-xs text-green-500">
               <img class="w-3 h-3" src="https://cdn-icons-png.flaticon.com/512/3334/3334338.png" alt="">
               <label for="">${v.point} Poins</label>
             </span>
           </div>
-          <div class="flex items-center w-full px-2 pb-2">
-            <div class="w-full">
+          <div class="flex justify-between w-full px-2 pb-2">
+            <div class="">
               <span class="fa fa-star checked"></span>
               <span class="fa fa-star checked"></span>
               <span class="fa fa-star checked"></span>
@@ -43,7 +45,7 @@ const sendCard = (v) => {
               <span class="fa fa-star"></span><br>
               <span class="text-xs text-gray-300">160 reviews</span>
             </div>
-            <div class="">
+            <div class="pr-6">
               <img class="h-6 w-6" src="https://cdn-icons-png.flaticon.com/512/535/535285.png" alt="">
               <!-- https://cdn-icons-png.flaticon.com/512/535/535183.png -->
             </div>
